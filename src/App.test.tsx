5e616977a4bfde7bb6f0eb145453a1c9881bff72
thoufeekx <thoufeekbaber1@gmail.com>
2025-04-08 22:32:53 -0400

    Update test to match Credit Risk Predictor content

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders credit risk predictor heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Credit Risk Predictor/i);
+  expect(headingElement).toBeInTheDocument();
 });
